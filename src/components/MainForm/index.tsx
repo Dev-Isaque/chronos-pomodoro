275ad07e5e49bd0bbd243a02b490845e649d5bbe
Dev-Isaque <isaquesbl123@gmail.com>
2026-01-08 23:11:22 -0300

    Validando o formulário e criando uma nova tarefa

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,16 +2,52 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
-import { useRef, useState } from 'react';
+import { useRef /*  useState  */ } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext';
 
 export function MainForm() {
-  const [taskName, setTaskName] = useState('');
+  /*   const [taskName, setTaskName] = useState(''); */
+  const { setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    console.log('Criando nova tarefa:', taskName, taskNameInput.current?.value);
+    if (taskNameInput.current == null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Por favor, insira um nome para a tarefa.');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptedDate: null,
+      duration: 25 * 60, // Duração padrão de 25 minutos
+      type: 'workTime',
+    };
+
+    const secondsRaemaining = newTask.duration * 60; 
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, // Conferir 
+        secondsRaemaining, // Conferir 
+        formattedSecondsRemaining: '00:00',  // Conferir 
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+
+    console.log('Criando nova tarefa: ', taskName);
   }
 
   return (
@@ -22,7 +58,7 @@ export function MainForm() {
           id='meuInput'
           type='text'
           placeholder='Digite algo'
-     /*      value={taskName}
+          /*      value={taskName}
           onChange={e => setTaskName(e.target.value)} */
           ref={taskNameInput}
         />
